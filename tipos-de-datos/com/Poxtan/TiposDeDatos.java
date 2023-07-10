73a3b2138a2415afe052b993e5c33105667c6e83
Beto Poxtan <110854400+codingByPoxtan@users.noreply.github.com>
2023-07-09 23:39:23 -0600

    Add files via upload

---
+++ tipos-de-datos/com/Poxtan/TiposDeDatos.java
@@ -0,0 +1,43 @@
+package com.Poxtan;
+
+public class TiposDeDatos {
+
+    public static void main(String[] args) {
+
+        //Numéricos
+
+        byte variable1 = 10;
+        System.out.println(variable1);
+
+        short variable2 = 15;
+        System.out.println(variable2);
+
+        int variable3 = 20;
+        System.out.println(variable3);
+
+        long variable4 = 500;
+        System.out.println(variable4);
+
+        float variable5 = 5.5f;
+        System.out.println(variable5);
+
+        double variable6 = 15.6d;
+        System.out.println(variable6);
+
+        //Booleanos
+
+        boolean variable7 = true;
+        System.out.println(variable7);
+
+        boolean variable8 = false;
+        System.out.println(variable8);
+
+        //Texto
+
+        char variable9 = 'a';
+        System.out.println(variable9);
+
+        String variable10 = "Hola mundo";
+        System.out.println(variable10);
+    }
+}
